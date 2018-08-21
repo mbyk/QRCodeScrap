ea0e5eb38c49334607c05e2e5f3feecbcfd6b574
mbyk <marubayashi@gmail.com>
2018-08-21 18:56:45 +0900

    convert tab indent to space indent

--- appserver/config/routes.rb
+++ appserver/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-	namespace :api, format: 'json' do
-		namespace :v1 do
-			post '/login', to: 'session#create'
-			post '/signup', to: 'users#create'
-			post '/api_token_validate', to: 'session#validate_api_token'
-		end
-	end
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      post '/login', to: 'session#create'
+      post '/signup', to: 'users#create'
+      post '/api_token_validate', to: 'session#validate_api_token'
+    end
+  end
 
 end
