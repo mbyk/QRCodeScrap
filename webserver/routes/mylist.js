b57b283771b552dac604ee3c1d6701d5bb2ae75e
mbyk <marubayashi@gmail.com>
2018-08-23 14:24:51 +0900

    show mylists's api data

--- webserver/routes/mylist.js
+++ webserver/routes/mylist.js
@@ -3,15 +3,50 @@
 const express = require('express');
 const router = express.Router();
 const parseUser = require('./parseUser');
+const axios = require('axios');
 
 router.get('/', (req, res, next) => {
 
   const user = parseUser(req);
 
-  res.render('mylist', {
-    title: 'マイリスト',
-    user: user
-  });
+  if (!user) {
+    res.redirect('/');
+    return;
+  }
+
+  const apiToken = req.session.apiToken;
+
+  const http = axios.create({
+    baseURL: `http://localhost:8000/api/v1`
+  })
+  http.interceptors.request.use((config) => {
+    if (apiToken) {  
+      config.headers.Authorization = `Bearer ${apiToken}`
+      return config
+    }
+    return config
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+  http.get('/mylist')
+    .then((res) => res.data)
+    .then((json) => {
+      console.log(`${JSON.stringify(json)}`)
+      res.render('mylist', {
+        title: 'マイリスト',
+        user: user,
+        qrcodes: json.results
+      });
+    })
+    .catch((err) => {
+     res.render('mylist', {
+        title: 'マイリスト',
+        user: user,
+        qrcodes: [] 
+      });
+    });
+
 });
 
 module.exports = router;
