4307d6ce983f662b8d5ab0a6e872e0fe3688f363
mbyk <marubayashi@gmail.com>
2018-08-23 17:25:14 +0900

    fix mylist  fetch logic

--- appserver/app/controllers/api/v1/mylist_controller.rb
+++ appserver/app/controllers/api/v1/mylist_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::MylistController < ApplicationController
   before_action :authenticate_request!
 
   def list
-    mylists = Mylist.all
+    email = @decoded_api_token[:email]
+    user = User.find_by(email: email)
+    mylists = Mylist.where(user_id: user.id)
     qrcodeIds = mylists.map(&:qrcode_id)
     qrcodes = Qrcode.qrcodes_in(qrcodeIds, :DESC)
     render json: {
