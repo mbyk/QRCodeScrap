80d9290ac18a22aa751f3a5e6374d7f06059665b
mbyk <marubayashi@gmail.com>
2018-09-03 19:04:49 +0900

    add webpack.productin.js for production env

---
+++ webserver/webpack.production.js
@@ -0,0 +1,112 @@
+const webpack = require('webpack')
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './webfront/index',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './public/javascripts')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [['env', {
+            'modules': false
+          }]]
+        }
+
+      }, {
+        test: /\.(scss|css)$/,
+
+        use: [
+          {
+            loader: 'vue-style-loader'
+          },
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }]
+      }]
+  },
+
+  resolve: {
+
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+
+  },
+
+  mode: 'production',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+  ]
+}
